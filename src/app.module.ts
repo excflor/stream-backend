4d9ea3e546651a9e2786f818a152f218313aed9f
andre.andre <andre@ukur.com>
2023-11-19 11:03:55 +0700

    feat: add env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CustomerModule } from './domain/customer/customer.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CustomerModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+      isGlobal: true,
+    }),
+    CustomerModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
